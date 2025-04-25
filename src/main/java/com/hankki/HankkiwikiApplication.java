3a794e336d6887879e3a104c451341bcdf97a200
marinrim <marinrim23@gmail.com>
2025-04-25 16:38:16 +0900

    :tada:Update Spring Boot Project

---
+++ src/main/java/com/hankki/HankkiwikiApplication.java
@@ -0,0 +1,13 @@
+package com.hankki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HankkiwikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HankkiwikiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/disiwiki/DishwikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.disiwiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DishwikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
