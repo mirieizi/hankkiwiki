41e541b43c8ed02cc5cd9ae83b7334b3d1412a80
mirim <marinrim23@gmail.com>
2025-05-09 16:43:16 +0900

    refactor: Update HttpStatus code

--- src/main/java/com/hankki/common/exception/ExceptionStatus.java
+++ src/main/java/com/hankki/common/exception/ExceptionStatus.java
@@ -10,9 +10,9 @@ public enum ExceptionStatus {
 	// DIET
 	INVALID_MEAL_TYPE(HttpStatus.BAD_REQUEST, 400, "유효하지 않은 식사 종류입니다."),
 	INVALID_MAJOR_CATEGORY(HttpStatus.BAD_REQUEST, 400, "유효하지 않은 대분류입니다."),
-	NOT_FOUND_DIET(HttpStatus.BAD_REQUEST, 400, "식사 정보가 존재하지 않습니다."),
-	NOT_FOUND_DIET_MEAL_ITEM(HttpStatus.BAD_REQUEST, 400, "식사에 대응하는 음식 정보를 찾지 못했습니다."),
-	NOT_FOUND_MEAL_ITEM(HttpStatus.BAD_REQUEST, 400, "음식 정보가 존재하지 않습니다."),
+	NOT_FOUND_DIET(HttpStatus.NOT_FOUND, 404, "식사 정보가 존재하지 않습니다."),
+	NOT_FOUND_DIET_MEAL_ITEM(HttpStatus.NOT_FOUND, 404, "식사에 대응하는 음식 정보를 찾지 못했습니다."),
+	NOT_FOUND_MEAL_ITEM(HttpStatus.NOT_FOUND, 404, "음식 정보가 존재하지 않습니다."),
 
 	// DB
 	FAIL_TO_CREATE_ENTITY(HttpStatus.BAD_REQUEST, 400, "엔티티 생성에 실패했습니다.");
