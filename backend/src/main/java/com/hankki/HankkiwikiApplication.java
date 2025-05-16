2e116e8d47c5934ccc921bf110ccd36b705166dc
eizi <eizi@gmail.com>
2025-05-16 12:21:10 +0900

    [fix] docker make.file and some mysql settings

--- backend/src/main/java/com/hankki/HankkiwikiApplication.java
+++ backend/src/main/java/com/hankki/HankkiwikiApplication.java
@@ -3,7 +3,8 @@ package com.hankki;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
+@SpringBootApplication(scanBasePackages = {"com.hankki"})
 public class HankkiwikiApplication {
 
 	public static void main(String[] args) {
