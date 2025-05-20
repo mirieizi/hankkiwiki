3f1ec28c889cf39fb022e8e09a2bcf4d650a689f
eizi <eizi@gmail.com>
2025-05-20 09:35:32 +0900

    [fix] refreshtokenRepository add @Modifying annotation

--- backend/src/main/java/com/hankki/domain/auth/repository/RefreshTokenRepository.java
+++ backend/src/main/java/com/hankki/domain/auth/repository/RefreshTokenRepository.java
@@ -3,6 +3,7 @@ package com.hankki.domain.auth.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 
 import com.hankki.domain.auth.entity.RefreshToken;
 
@@ -11,8 +12,10 @@ import jakarta.transaction.Transactional;
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long>{
 	Optional<RefreshToken> findByUserId(Long userId);
 	Optional<RefreshToken> findByRefreshToken(String refreshToken);
+	@Modifying
 	@Transactional
 	void deleteByRefreshToken(String refreshToken);
+	@Modifying
 	@Transactional
 	void deleteByUserId(Long userId);
 
